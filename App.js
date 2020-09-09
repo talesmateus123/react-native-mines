4f0e8e7aab12216da38a83fe49f3f9e5717c6f32
Tales Ferreira <talesmateus1999@hotmail.com>
2020-09-09 12:01:24 -0400

    Implemented header; Implemented level selection modal.

--- App.js
+++ App.js
@@ -4,10 +4,22 @@ import {
   StyleSheet,
   Text,
   View,
+  Alert
 } from 'react-native';
 import params from './src/params';
 import MineField from './src/components/MineField';
-import { createMinedBoard } from './src/functions'
+import Header from './src/components/Header'
+import LevelSelection from './src/screens/LevelSelection'
+import { 
+  createMinedBoard, 
+  cloneBoard, 
+  openField, 
+  hadExplosion, 
+  wonGame, 
+  showMines, 
+  invertFlag,
+  flagsUsed
+} from './src/functions'
 
 export default class App extends Component {
 
@@ -22,27 +34,68 @@ export default class App extends Component {
     return Math.ceil(cols * rows * params.difficultLevel)
   }
 
-  createState= () => {
+  createState = () => {
     const cols = params.getColumnsAmount()
     const rows = params.getRowsAmount()
     return {
-      board: createMinedBoard(rows, cols, this.minesAmount())
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
+      showLevelSelection: false
     }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Opps...', 'Você perdeu')
+    }
+
+    if (won) {
+      Alert.alert('Parabéns!', 'Você venceu')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board, row, column)
+
+    const won = wonGame(board)
+
+    if (won) {
+      Alert.alert('Parabéns!', 'Você venceu')
+    }
+
+    this.setState({ board, won })
+  }
+
+  onLevelSelected = level => {
+    params.difficultLevel = level
+    this.setState(this.createState())
   }
 
   render() {
     return (
-      <>
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.welcome}>Starting the mines</Text>
-          <Text style={styles.instructions}>
-            { `Grid size: ${params.getRowsAmount()}x${params.getColumnsAmount()}` }
-          </Text>
-          <View style={styles.board}>
-            <MineField board={this.state.board}/>
-          </View>
-        </SafeAreaView>
-      </>
+      <SafeAreaView style={styles.container}>
+        <LevelSelection visible={this.state.showLevelSelection} 
+          onLevelSelected={this.onLevelSelected}
+          onCancel={() => this.setState({ showLevelSelection: false })} />
+        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} 
+          onNewGame={() => this.setState(this.createState())}
+          onFlagPress={() => this.setState({ showLevelSelection: true })} />
+        <View style={styles.board}>
+          <MineField board={this.state.board} 
+          onOpenField={this.onOpenField}
+          onSelectField={this.onSelectField} />
+        </View>
+      </SafeAreaView>
     );
   }
 };
@@ -52,14 +105,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   board: {
     alignItems: 'center',
-    backgroundColor: '#AAA'
+    padding: 10
   },
   welcome: {
     fontSize: 20,
+    fontWeight: 'bold',
     textAlign: 'center'
   }
 });
